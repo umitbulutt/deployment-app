028771e9deef54b2f111cfd7efd7d2258ad8f206
CundullahT-CT <88734775+CundullahT-CT@users.noreply.github.com>
2024-08-11 21:56:13 +0200

    Update FakeUserServiceImpl.java

--- src/main/java/com/cydeo/fakeuserapp/service/impl/FakeUserServiceImpl.java
+++ src/main/java/com/cydeo/fakeuserapp/service/impl/FakeUserServiceImpl.java
@@ -1,21 +1,32 @@
 package com.cydeo.fakeuserapp.service.impl;
 
+import com.cydeo.fakeuserapp.repository.FakeUserRepository;
 import com.cydeo.fakeuserapp.service.FakeUserService;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class FakeUserServiceImpl implements FakeUserService {
 
     private final Faker faker;
+    private final FakeUserRepository fakeUserRepository;
 
-    public FakeUserServiceImpl(Faker faker) {
+    public FakeUserServiceImpl(Faker faker, FakeUserRepository fakeUserRepository) {
         this.faker = faker;
+        this.fakeUserRepository = fakeUserRepository;
     }
 
     @Override
     public String getRandomName() {
         return faker.name().fullName();
+    }
+
+    @Override
+    public String getNameFromDB() {
+        Long id = (long) new Random().nextInt(10) + 1;
+        return fakeUserRepository.findById(id).get().getName();
     }
 
 }
