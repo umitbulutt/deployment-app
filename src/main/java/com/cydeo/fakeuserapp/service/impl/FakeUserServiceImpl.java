1d4bc3af7640815a8789fc0e5bc70d88975af7b7
CundullahT-CT <88734775+CundullahT-CT@users.noreply.github.com>
2024-08-11 21:47:50 +0200

    Update FakeUserServiceImpl.java

--- src/main/java/com/cydeo/fakeuserapp/service/impl/FakeUserServiceImpl.java
+++ src/main/java/com/cydeo/fakeuserapp/service/impl/FakeUserServiceImpl.java
@@ -1,1 +1,21 @@
+package com.cydeo.fakeuserapp.service.impl;
 
+import com.cydeo.fakeuserapp.service.FakeUserService;
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FakeUserServiceImpl implements FakeUserService {
+
+    private final Faker faker;
+
+    public FakeUserServiceImpl(Faker faker) {
+        this.faker = faker;
+    }
+
+    @Override
+    public String getRandomName() {
+        return faker.name().fullName();
+    }
+
+}
